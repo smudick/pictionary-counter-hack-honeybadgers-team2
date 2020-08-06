0ff01f0da372a22a220403e1444f46d08b1b7d8e
Sam Mudick <smudick@gmail.com>
2020-08-05 21:56:31 -0500

    added color changing depending on score

--- setup/app.js
+++ setup/app.js
@@ -15,23 +15,36 @@ const buttonEvents = () => {
 const increaseScore = () => {
     score += 1;
     printToDom('score', score);
+    colorChange();
     return score;
 }
 
 const decreaseScore = () => {
     score -= 1;
     printToDom('score', score);
+    colorChange();
     return score;
 }
 
 const resetScore = () => {
     score = 0;
     printToDom('score', score);
+    colorChange();
     return score;
 }
 
+const colorChange = () => {
+    if (score > 0) {
+        document.querySelector('#score').style.color = '#008000';
+    } else if (score < 0) {
+        document.querySelector('#score').style.color = '#FF0000';
+    } else {
+        document.querySelector('#score').style.color = '#000000';
+    }
+}
+
 const init = () => {
-    buttonEvents();
+    buttonEvents(); 
 }
 
 init();
