675b4a410877ac2f6e220fa3501e0d7e4b79f61f
Sam Mudick <smudick@gmail.com>
2020-08-05 23:17:35 -0500

    added second functional scorecard

--- setup/app.js
+++ setup/app.js
@@ -1,50 +1,86 @@
-'use strict'
-let score = 0;
+"use strict";
+
+let score1 = 0;
+let score2 = 0;
 
 const printToDom = (divId, textToPrint) => {
-    const selectedDiv = document.getElementById(divId);
-    selectedDiv.innerHTML = textToPrint;
-  };
-
-const buttonEvents = () => {
-    document.querySelector("#increaseButton").addEventListener("click", increaseScore);
-    document.querySelector("#decreaseButton").addEventListener("click", decreaseScore);
-    document.querySelector("#resetButton").addEventListener("click", resetScore);
+  const selectedDiv = document.getElementById(divId);
+  selectedDiv.innerHTML = textToPrint;
 };
 
-const increaseScore = () => {
-    score += 1;
-    printToDom('score', score);
-    colorChange();
-    return score;
-}
+const buttonEvents = () => {
+  document
+    .querySelector("#increaseButton")
+    .addEventListener("click", increaseScore1);
+  document
+    .querySelector("#increaseButton2")
+    .addEventListener("click", increaseScore2);
+  document
+    .querySelector("#decreaseButton")
+    .addEventListener("click", decreaseScore1);
+  document
+    .querySelector("#decreaseButton2")
+    .addEventListener("click", decreaseScore2);
+  document.querySelector("#resetButton").addEventListener("click", resetScore1);
+  document
+    .querySelector("#resetButton2")
+    .addEventListener("click", resetScore2);
+};
 
-const decreaseScore = () => {
-    score -= 1;
-    printToDom('score', score);
-    colorChange();
-    return score;
-}
+const increaseScore1 = () => {
+  score1 += 1;
+  printToDom("scoreOne", score1);
+  colorChange(score1, "scoreOne");
+  return score1;
+};
 
-const resetScore = () => {
-    score = 0;
-    printToDom('score', score);
-    colorChange();
-    return score;
-}
+const increaseScore2 = () => {
+  score2 += 1;
+  printToDom("scoreTwo", score2);
+  colorChange(score2, "scoreTwo");
+  return score2;
+};
 
-const colorChange = () => {
-    if (score > 0) {
-        document.querySelector('#score').style.color = '#008000';
-    } else if (score < 0) {
-        document.querySelector('#score').style.color = '#FF0000';
-    } else {
-        document.querySelector('#score').style.color = '#000000';
-    }
-}
+const decreaseScore1 = () => {
+  score1 -= 1;
+  printToDom("scoreOne", score1);
+  colorChange(score1, "scoreOne");
+  return score1;
+};
+
+const decreaseScore2 = () => {
+  score2 -= 1;
+  printToDom("scoreTwo", score2);
+  colorChange(score2, "scoreTwo");
+  return score2;
+};
+
+const resetScore1 = () => {
+  score1 = 0;
+  printToDom("scoreOne", score1);
+  colorChange(score1, "scoreOne");
+  return score1;
+};
+
+const resetScore2 = () => {
+  score2 = 0;
+  printToDom("scoreTwo", score2);
+  colorChange(score2, "scoreTwo");
+  return score2;
+};
+
+const colorChange = (value, id) => {
+  if (value > 0) {
+    document.getElementById(id).style.color = "#008000";
+  } else if (value < 0) {
+    document.getElementById(id).style.color = "#FF0000";
+  } else {
+    document.getElementById(id).style.color = "#000000";
+  }
+};
 
 const init = () => {
-    buttonEvents(); 
-}
+  buttonEvents();
+};
 
 init();
